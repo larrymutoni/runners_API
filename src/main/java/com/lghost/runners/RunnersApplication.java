0b88049af29b0301da7ebf5ca09ba8d4ef2657c2
larrymutoni <larrycarnot@gamil.com>
2024-04-16 17:15:41 +0300

    Initial commit

---
+++ src/main/java/com/lghost/runners/RunnersApplication.java
@@ -0,0 +1,13 @@
+package com.lghost.runners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lghost/runners/RunnersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lghost.runners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
